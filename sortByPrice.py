58872ea8c600e178e3912921c6cb03c497e7265e
rickBucket <zrickyzhao@gmail.com>
2020-01-09 15:36:28 -0800

    Initial file uploads
    Code and input file with sample data

---
+++ sortByPrice.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+import json
+
+
+outputData = []
+
+# Read input file and add keys
+with open("input.json") as json_file:
+    inputFile = json.load(json_file)
+    for line in inputFile:
+    	outputData.append({
+    		"name": line[0],
+    		"color": line[1],
+    		"price": line[2],
+    		"storage": line[3],
+    		"rating": line[4],
+    		"url": line[5]
+    	})
+
+# Sort by prices without the "$" and as ints
+outputData.sort(key=lambda s: int(s["price"][1:]))
+
+# Output into file with given formatting
+with open("output.json", "w") as outfile:
+	outfile.write("[" + "\n")
+	for index in range(len(outputData)):
+		outfile.write("{\"name\":")
+		outfile.write(json.dumps(outputData[index]["name"]))
+		outfile.write(", \"color\":")
+		outfile.write(json.dumps(outputData[index]["color"]))
+		outfile.write(", \"price\":")
+		outfile.write(json.dumps(outputData[index]["price"]))
+		outfile.write(", \"storage\":")
+		outfile.write(json.dumps(outputData[index]["storage"]))
+		outfile.write(", \"rating\":")
+		outfile.write(json.dumps(outputData[index]["rating"]))
+		outfile.write(", \"url\":")
+		outfile.write(json.dumps(outputData[index]["url"]))
+		if index+1 == len(outputData):
+			outfile.write("}")
+		else: 
+			outfile.write("},")
+		outfile.write("\n")
+	outfile.write("]")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
